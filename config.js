5f6b75f1933385bdd5715ce84c877bfeb51f7b08
César Encina Leon <ceel2004@hotmail.com>
2015-11-04 00:06:55 -0300

    Add Config

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   // your community or team name to display on join page.
-  community: process.env.COMMUNITY_NAME || 'YOUR-TEAM-NAME',
+  community: process.env.COMMUNITY_NAME || 'IV Devs',
   // your slack team url (ex: socketio.slack.com)
-  slackUrl: process.env.SLACK_URL || 'YOUR-TEAM.slack.com',
+  slackUrl: process.env.SLACK_URL || 'ivdevs.slack.com',
   // access token of slack
   // You can generate it in https://api.slack.com/web#auth
   // You should generate the token in admin user, not owner.
@@ -10,7 +10,7 @@ module.exports = {
   //
   // You can test your token via curl:
   //   curl -X POST 'https://YOUR-SLACK-TEAM.slack.com/api/users.admin.invite' \
-  //   --data 'email=EMAIL&token=TOKEN&set_active=true' \
+  //   --data 'email=EMAIL&token=xoxp-13346603558-13350392466-13833830320-da062f28dd&set_active=true' \
   //   --compressed
-  slacktoken: process.env.SLACK_TOKEN || 'YOUR-ACCESS-TOKEN'
+  slacktoken: process.env.SLACK_TOKEN || 'xoxp-13346603558-13350392466-13833830320-da062f28dd'
 };
